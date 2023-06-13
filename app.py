caafc9396c3fe27fc248b53ae54ea52d789dcad1
agniiiva <mr.agniiiva@gmail.com>
2023-06-14 02:18:46 +0530

    Commit

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from bardapi import Bard
 import os
 
 app = Flask(__name__)
-os.environ['_BARD_API_KEY'] = "XQgHISTD3YxeDpORSZNcxd9N3yE1H33_TETWss3ZiKPyIkvcHVuSkdBEQOWhWKaswoPaUw."
+os.environ['_BARD_API_KEY'] = "XQgHIS1eV0wute1ljXTden_kOX87ZRQHyTEMUbwyNK0j4lQZMw3QcteOkXjpQUP19hfykA."
+bard_inproxy = Bard(timeout=10)
 
 @app.route('/api', methods=['POST'])
 def get_bard_content():
-    input_text = request.form.get('input')
+    input_text = request.form.get('input')-
     content = Bard().get_answer(input_text)['content']
     return jsonify({"content": content})
 
